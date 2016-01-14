bd6f60787f8476efcc9ee0ccafa40fbab6a796ec
emartin <ewan.martin@vigicorp.fr>
2016-01-14 12:50:10 +0100

    Add 'schema' to Get

--- Libraries/PrestaShopWebService.php
+++ Libraries/PrestaShopWebService.php
@@ -283,7 +283,7 @@ class PrestaShopWebService
 			if (isset($options['id']))
 				$url .= '/'.$options['id'];
 				
-			$params = array('filter', 'display', 'sort', 'limit', 'id_shop', 'id_group_shop');
+			$params = array('filter', 'display', 'sort', 'limit', 'id_shop', 'id_group_shop', 'schema');
 			foreach ($params as $p)
 				foreach ($options as $k => $o)
 					if (strpos($k, $p) !== false)
